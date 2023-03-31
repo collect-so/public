fb8c4bdd2c20fd41fa1e757ca126db0d9dc2cf5d
Artemiy Vereshchinskiy <artemiy.vereshchinskiy@gmail.com>
2023-03-31 23:16:14 +0400

    Fix empty file

--- src/Sections/Hero/index.tsx
+++ src/Sections/Hero/index.tsx
@@ -1,11 +1,30 @@
 import { Section } from "@common/section";
-
+import rocket from "~/images/rocket.png";
+import lightning from "~/images/lightning.png";
+import Image from "next/image";
+import classNames from "classnames";
 export function HeroSection() {
   return (
     <Section
       className="min-h-screen mt-[70px] md:mt-[60px] grid place-content-center"
       data-theme="light"
     >
+      <Image
+        src={lightning}
+        alt={"lightning"}
+        className={classNames(
+          "sm:hidden absolute left-0 top-[200px] z-0",
+          "md:right-[-60px]",
+        )}
+      />
+      <Image
+        src={rocket}
+        alt={"rocket"}
+        className={classNames(
+          "sm:hidden absolute right-0 bottom-0 z-0",
+          "md:right-[-60px]",
+        )}
+      />
       <div className="container">
         <div className="basis-1/1 text-center">
           <h2 className="text-3xl font-bold mb-8 text-content-primary-light sm:text-xl ">
