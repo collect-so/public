16588b668de514d19fb2de3bf164b2481019a1d9
h3yAlias <asthmaisback@gmail.com>
2023-04-02 17:50:02 +0400

    fix: nodemailer reply, regex for email, small refactor

--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -1,10 +1,10 @@
-import nodemailer from 'nodemailer'
+import nodemailer from "nodemailer"
 
 const emailUser = process.env.EMAIL_USER;
 const emailUserPw = process.env.EMAIL_USER_PASSWORD
 
 export const transport = nodemailer.createTransport({
-    service: 'gmail',
+    service: "gmail",
     auth: {
         user: emailUser,
         pass: emailUserPw
--- src/pages/api/email.ts
+++ src/pages/api/email.ts
@@ -2,20 +2,23 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import {Data, FeedbackFormDto, transport, transportOptions} from "../../config/nodemailer";
 
+const baseEmail = /^\S.*@\S+$/
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const requestData: FeedbackFormDto = req.body
 
-    if (!requestData.name || !requestData.email) {
+    if (!requestData.name || !requestData.email || (requestData.email && !baseEmail.test(requestData.email))) {
       return res.status(400).json({ success: false })
     }
 
     try {
       await transport.sendMail({
         ...transportOptions,
+        replyTo: requestData.email,
         subject: `Request to join the waiting list from ${requestData.name}`,
         html: `
           <h1>
