0198067b9bbb71d1be5e727f8137fb58775c509b
h3yAlias <asthmaisback@gmail.com>
2023-04-02 17:42:20 +0400

    feat: added nodemailer api

---
+++ src/config/nodemailer.ts
@@ -0,0 +1,29 @@
+import nodemailer from 'nodemailer'
+
+const emailUser = process.env.EMAIL_USER;
+const emailUserPw = process.env.EMAIL_USER_PASSWORD
+
+export const transport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: emailUser,
+        pass: emailUserPw
+    },
+}, {
+    from: emailUser,
+    replyTo: emailUser
+})
+
+export const transportOptions = {
+    to: emailUser
+}
+
+export interface FeedbackFormDto {
+    email: string;
+    name: string;
+}
+
+export type Data = {
+    success: boolean;
+    message?: string;
+};
--- src/pages/api/email.ts
+++ src/pages/api/email.ts
@@ -1,13 +1,37 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import {Data, FeedbackFormDto, transport, transportOptions} from "../../config/nodemailer";
 
-type Data = {
-  success: boolean;
-};
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  res.status(200).json({ success: true });
+  if (req.method === 'POST') {
+    const requestData: FeedbackFormDto = req.body
+
+    if (!requestData.name || !requestData.email) {
+      return res.status(400).json({ success: false })
+    }
+
+    try {
+      await transport.sendMail({
+        ...transportOptions,
+        subject: `Request to join the waiting list from ${requestData.name}`,
+        html: `
+          <h1>
+             Hello! We've accepted a request to join the waitlist from ${requestData.name}
+          </h1>
+          <p>
+            Contact email is: ${requestData.email}        
+          </p>
+        `
+      })
+
+      res.status(200).json({ success: true });
+    }
+    catch (error) {
+      return res.status(500).json({ success: false, message: error as unknown as string })
+    }
+  }
+
 }
