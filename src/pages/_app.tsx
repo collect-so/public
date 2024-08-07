da3c2e42d84804f59155c202a20a41869f4054a4
Erik Farukshin <nctilucent@gmail.com>
2024-08-07 16:01:34 +0200

    add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,8 +7,11 @@ import { jetBrainsMono, kyivTypeSans, manrope } from "~/styles/fonts"
 import { useRef } from "react"
 import cx from "classnames"
 
+const isProd = process.env.NODE_ENV === "production"
+
 export default function App({ Component, pageProps }: AppProps) {
   const ref = useRef<HTMLElement>(null)
+
   return (
     <>
       <style jsx global>{`
@@ -41,8 +44,14 @@ export default function App({ Component, pageProps }: AppProps) {
         ref={ref}
       >
         <Component {...pageProps} />
-        {/*<CustomCursor ref={ref} />*/}
       </main>
+      {isProd && (
+        <Script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="3cbd0988-2578-48fa-a572-8593d2e2b8cd"
+        />
+      )}
     </>
   )
 }
